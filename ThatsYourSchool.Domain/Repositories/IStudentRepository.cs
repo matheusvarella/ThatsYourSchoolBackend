30067d5ee026d38c25d3edde78e3cd41b21cc877
Matheus Varella <varellamat@gmail.com>
2023-03-18 16:18:21 -0300

    Added the repository pattern

---
+++ ThatsYourSchool.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,9 @@
+﻿using ThatsYourSchool.Domain.Entities;
+
+namespace ThatsYourSchool.Domain.Repositories;
+
+public interface IStudentRepository
+{
+    void Create(Student student);
+    void Update(Student student);
+}
